084e464137d52bc124fac78d6923279fef2b178b
gcy <gu1070398201@sina.com>
2019-07-31 21:08:55 +0800

    提交文件

--- src/main/java/com/sb/controller/UserController.java
+++ src/main/java/com/sb/controller/UserController.java
@@ -3,9 +3,10 @@ package com.sb.controller;
 import com.sb.bean.User;
 import com.sb.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -17,5 +18,25 @@ public class UserController {
     public User findInfoId(@PathVariable Integer id){
 
         return mapper.findIdInfo(id);
+    }
+
+    @RequestMapping(value = "/addinfo")
+    public String  addInfo(@RequestParam String name,
+                           @RequestParam String upsd){
+        User user=new User();
+        user.setUname(name);
+        user.setUpsd(upsd);
+        int tyoe=mapper.addInfo(user);
+        if (tyoe==1){
+            return "新增成功！";
+        }else{
+            return "新增失败！";
+        }
+    }
+    @RequestMapping(value = "/allSelect")
+    public List<User> allSelect(){
+        List<User> list=new ArrayList<>();
+        list=mapper.allSelect();
+        return list;
     }
 }
--- src/main/java/com/sb/mapper/UserMapper.java
+++ src/main/java/com/sb/mapper/UserMapper.java
@@ -1,12 +1,23 @@
 package com.sb.mapper;
 
 import com.sb.bean.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
 
     @Select("select * from user_login where id=#{id}")
     public User findIdInfo(Integer id);
+
+    @Insert("insert into user_login (uname,upsd) values(#{user.uname},#{user.upsd})")
+    public int addInfo(@Param("user") User user);
+
+    @Select("select * from user_login")
+    public List<User> allSelect();
+
 }
